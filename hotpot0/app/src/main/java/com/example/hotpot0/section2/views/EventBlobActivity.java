bfa7bbb2ff93db616c88279f9e3e89957b528024
Sejal Liju <sejallij@ualberta.ca>
2025-11-06 20:01:30 -0700

    EventBlob Logic

--- hotpot0/app/src/main/java/com/example/hotpot0/section2/views/EventBlobActivity.java
+++ hotpot0/app/src/main/java/com/example/hotpot0/section2/views/EventBlobActivity.java
@@ -15,11 +15,11 @@ public class EventBlobActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.events);
+        setContentView(R.layout.event_blob);
 
         eventName = findViewById(R.id.event_name);
         eventRole = findViewById(R.id.event_role);
-//        arrowIcon = findViewById(R.id.event_role); // optional, adjust if you want click behavior
+        arrowIcon = findViewById(R.id.event_arrow);
 
         // Retrieve and display data (if passed)
         Bundle extras = getIntent().getExtras();
