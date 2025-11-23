5988a72f066151177f237831cedc3db7bd6fd398
Abhishekh Jayaprakash <ajayapra@ualberta.ca>
2025-11-22 18:17:50 -0700

    Fixed toast messages when user tries to join a filled event

--- hotpot0/app/src/main/java/com/example/hotpot0/section2/views/EventInitialActivity.java
+++ hotpot0/app/src/main/java/com/example/hotpot0/section2/views/EventInitialActivity.java
@@ -76,7 +76,6 @@ public class EventInitialActivity extends AppCompatActivity {
         Spinner eventDetailsSpinner = findViewById(R.id.EventDetailsSpinner);
         Context activityContext = this;
 
-
         // Fetch the event details from EventDB
         eventDB.getEventByID(eventID, new EventDB.GetCallback<Event>() {
             @Override
@@ -256,8 +255,20 @@ public class EventInitialActivity extends AppCompatActivity {
                             eventHandler.joinWaitList(userID, eventID, new ProfileDB.GetCallback<Integer>() {
                                 @Override
                                 public void onSuccess(Integer result) {
-                                    Toast.makeText(EventInitialActivity.this, "Successfully joined the waitlist!", Toast.LENGTH_SHORT).show();
-                                    navigateHome();
+                                    switch (result) {
+                                        case 0: // Successfully added to waitlist
+                                            Toast.makeText(EventInitialActivity.this, "Successfully joined the waitlist!", Toast.LENGTH_SHORT).show();
+                                            navigateHome();
+                                            break;
+                                        case 1:
+                                            Toast.makeText(EventInitialActivity.this, "You are already affiliated with this event.", Toast.LENGTH_SHORT).show();
+                                            break;
+                                        case 2: // Waitlist full
+                                            Toast.makeText(EventInitialActivity.this, "Waitlist is full, cannot join!", Toast.LENGTH_SHORT).show();
+                                            break;
+                                        default:
+                                            Toast.makeText(EventInitialActivity.this, "Unknown status: " + result, Toast.LENGTH_SHORT).show();
+                                    }
                                 }
 
                                 @Override
